7b3b3e85929e732153b578b81be57a838e6e3344
CarmenDAM <cceballose01@educantabria.es>
2022-03-06 17:54:28 +0100

    1 file modified, 14 added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,24 @@
-import React from "react";
-import "./style.css";
+import React from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import Menu from './components/Menu'
+import { PaginasApp } from './data/PaginasApp';
 
-export default function App() {
+function App() {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
-    </div>
+    <Router>
+      <Menu />
+      {PaginasApp.map((item) => {
+        return (
+          <Route
+            key={item.id}
+            path={item.path}
+            exact
+            component={item.component}
+          />
+        );
+      })}
+    </Router>
   );
 }
+
+export default App;
